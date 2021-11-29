1c70ddc57f1020288eba7d75fa399acb5146c22e
waldir sanchez <waldir.sanchez.p@gmail.com>
2021-11-29 06:32:01 -0500

    add config registry-server

---
+++ registry-service/src/test/java/wcsp/dev/registry/RegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package wcsp.dev.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

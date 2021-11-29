7063e8045b2603b12b95cd208b4f0b974fb93f2c
waldir sanchez <waldir.sanchez.p@gmail.com>
2021-11-29 09:15:30 -0500

    update files

--- service-desktop/src/main/java/wcsp/dev/desktop/ServiceDesktopApplication.java
+++ service-desktop/src/main/java/wcsp/dev/desktop/ServiceDesktopApplication.java
@@ -2,8 +2,10 @@ package wcsp.dev.desktop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ServiceDesktopApplication {
 
 	public static void main(String[] args) {
